a96fd364881513829a5b234a47afa2029d3f6672
mroger <rene.mroger@gmail.com>
2019-06-25 23:09:49 +0000

    commiting middle assertion

--- lotide/middle.js
+++ lotide/middle.js
@@ -18,3 +18,21 @@ const assertArraysEqual = function(func, args, expected) {
   console.log((actual === expected) ? `✅✅✅  Assertion Passed:  ${actual}  ===  ${expected} --- Asserting : ${func.name}` :
     `🛑🛑🛑  Assertion Failed: ${actual}`);
 };
+
+const middle = function(arr) {
+  const midIndex = Math.ceil(arr.length / 2);
+  if (arr.length < 2) {
+    return [];
+  }
+  if (arr.length % 2 === 1) {
+    return [arr[midIndex]];
+  } else {
+    return [arr[midIndex - 1], arr[midIndex]];
+  }
+};
+
+
+assertArraysEqual(eqArrays, [middle([1, 2, 3, 4]), middle([1, 2, 3, 4])], true);
+assertArraysEqual(eqArrays, [middle([1, 2, 3, 4]), middle([1, 2, 3])], false);
+assertArraysEqual(eqArrays, [middle([]), middle([1, 2, 3])], false);
+
