49a4cfda6500186761c4d752da85ccb4666b6e82
mroger <rene.mroger@gmail.com>
2019-06-28 00:56:48 +0000

    commiting kata.js

--- stretch/kata.js
+++ stretch/kata.js
@@ -1,13 +1,11 @@
 /* eslint-disable no-prototype-builtins */
 
-const organizeInstructors = function(instructors, func) {
-  const listOfInstructors = {};
+const organizeInstructors = function(instructors) {
+  let listOfInstructors = {};
   //dynamically creating the object keys
   instructors.forEach(function(e) {
-    console.log(instructors[e]);
-    if (func(listOfInstructors, instructors[e])) {
+    if (!listOfInstructors.hasOwnProperty(e.course)) {
       listOfInstructors[e.course] = [];
-      console.log(`add ${listOfInstructors[e.console]}`);
     }
     //adding values to keys
     listOfInstructors[e.course].push(e.name);
@@ -15,17 +13,40 @@ const organizeInstructors = function(instructors, func) {
   return listOfInstructors;
 };
 
-console.log(organizeInstructors([
+const refOrganizeInstructors = function(instructors, func) {
+  let listOfInstructors = {};
+  //dynamically creating the object keys
+  instructors.forEach(function(e) {
+    if (!func(listOfInstructors, e.course)) {
+      if (!listOfInstructors[e.course]) {
+        listOfInstructors[e.course] = [];
+      }
+    }
+    //adding values to keys
+    listOfInstructors[e.course].push(e.name);
+  });
+  return listOfInstructors;
+};
+
+
+console.log(refOrganizeInstructors([
   { name: "Samuel", course: "iOS" },
   { name: "Victoria", course: "Web" },
   { name: "Karim", course: "Web" },
   { name: "Donald", course: "Web" }
-], (x, y) => !x.hasOwnProperty(x[y])));
+], (x, y) => {
+  console.log(x);
+  console.log(y);
+  console.log(x.hasOwnProperty(x[y]));
+  return x.hasOwnProperty(x[y]);
+}));
 
-// console.log(organizeInstructors([
-//   { name: "Brendan", course: "Blockchain" },
-//   { name: "David", course: "Web" },
-//   { name: "Martha", course: "iOS" },
-//   { name: "Carlos", course: "Web" }
-// ], (x) => typeof x === undefined));
+console.log('____________________________________');
+
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
 
